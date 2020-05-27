0c86f51d1e208dc66a54250424f151562e143240
opentlc-mgr <nmalvank@redhat.com>
2020-05-27 03:43:40 +0000

    fix

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -24,6 +24,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello world";
+        return "hello world 1";
     }
 }
