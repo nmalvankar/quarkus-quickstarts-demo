7e492d6fb0c9a9bb50b95e009ba752126544738c
Demo User <demo-user@demo.com>
2020-08-04 22:23:22 +0000

    test 2

--- getting-started/src/main/java/org/acme/getting/started/GreetingService.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingService.java
@@ -6,7 +6,7 @@ import javax.enterprise.context.ApplicationScoped;
 public class GreetingService {
 
     public String greeting(String name) {
-        //Comment for testing git commit
+        //Comment for testing git commit2
         return "hello " + name;
     }
 
